0ba9dcb9ddd281bee7c222580f8f01895461638f
howing12 <howing12@gmail.com>
2026-08-18 12:38:29 +0000

    fix: set html lang to zh-HK and add app icon (favicon)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh-HK" suppressHydrationWarning>
       <head>
         <script
           dangerouslySetInnerHTML={{
